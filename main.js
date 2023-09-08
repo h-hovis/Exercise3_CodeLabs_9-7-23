ca31455b1651631964c33a3b4dada5b89e9d45de
hslape <awinter71417@gmail.com>
2023-09-07 19:04:30 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function everyday() {
+    let firstDay = 'Thursday';
+
+    function givenDay() {
+        let secondDay = 'Friday';
+        console.log(firstDay);
+        console.log(secondDay);
+    }
+
+    givenDay();
+}
+
+everyday();
